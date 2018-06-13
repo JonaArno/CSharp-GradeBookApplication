b8d65a534edd73631df8fdf8bd60c30c1e3c0208
JonaArno <jonathan.arnoys@gmail.com>
2018-06-13 18:39:23 +0200

    TopPercent

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -16,8 +17,23 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5) throw new InvalidOperationException();
 
-            //here
+            var treshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var treshold2 = (int)Math.Ceiling(Students.Count * 0.4);
+            var treshold3 = (int) Math.Ceiling(Students.Count * 0.6);
+            var treshold4 = (int) Math.Ceiling(Students.Count * 0.8);
 
+            var allGradesDesc = Students
+                .OrderByDescending(e => e.AverageGrade)
+                .Select(e => e.AverageGrade)
+                .ToList();
+
+            if (allGradesDesc[treshold - 1] <= averageGrade) return 'A';
+            else if (allGradesDesc[treshold - 1] > averageGrade && allGradesDesc[treshold2 - 1] <= averageGrade)
+                return 'B';
+            else if (allGradesDesc[treshold2 - 1] > averageGrade &&
+                     allGradesDesc[treshold3 - 1] <= averageGrade) return 'C';
+            else if (allGradesDesc[treshold3 - 1] > averageGrade &&
+                     allGradesDesc[treshold4 - 1] <= averageGrade) return 'D';
             return 'F';
         }
     }
